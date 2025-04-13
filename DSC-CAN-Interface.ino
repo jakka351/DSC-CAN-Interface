b45c82acaba55979d93c0394982f452acb05c8cd
Tester Present Specialist Automotive Solutions <jakka351@outlook.com>
2025-04-13 10:19:02 +1000

    Update DSC-CAN-Interface.ino
    Working Code

--- DSC-CAN-Interface.ino
+++ DSC-CAN-Interface.ino
@@ -31,9 +31,8 @@
 //
 //                              Copyright (c) 2025 Tester Present Specialist Automotive Solutions
 //
-//
-//             Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-//             documentation files (the "Software"), to use, modify, and distribute the Software subject to the following conditions:
+//                              Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+//                              documentation files (the "Software"), to use, modify, and distribute the Software subject to the following conditions:
 //
 //          1. Attribution.
 //            - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -49,8 +48,7 @@
 //          3. Redistribution.
 //            - Redistributions of the Software, in source or binary form, must reproduce the above attribution, this license, and the disclaimer below 
 //              in the documentation and/or other materials provided with the distribution.
-//            - Any modifications made to the Software must be clearly marked as such, and the modified files must carry prominent notices stating that
-//              changes have been made.
+//            - Any modifications made to the Software must be clearly marked as such, and the modified files must carry prominent notices stating that changes have been made.
 //
 //          4. No Warranty.
 //            - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -66,88 +64,133 @@
 //            - This license constitutes the entire agreement between you and Tester Present Specialist Automotive Solutions regarding the Software 
 //              and supersedes any prior understandings or agreements, whether written or oral, regarding the subject matter herein.
 //
-//            
-//              By using, modifying, or distributing the Software, you agree to abide by all the terms and conditions of this license.
+//          By using, modifying, or distributing the Software, you agree to abide by all the terms and conditions of this license.
 //
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                             
-#include <SPI.h>
-#include "mcp_can.h"
-#include <mcp_can.h>
-#include <SPI.h>
-const int SPI_CS_PIN = 17;              // CANBed V1
-// const int SPI_CS_PIN = 3;            // CANBed M0
-// const int SPI_CS_PIN = 9;            // CAN Bus Shield
-MCP_CAN MIDSPEEDCAN(SPI_CS_PIN);       
-// Define the button pins
-const int buttonPins[4] = {2, 3, 4, 5};
+#include <Wire.h>
+#include <stdio.h>
+#include "canbed_dual.h"
+//////////////////////////////////////////////////////
+CANBedDual MIDSPEEDCAN(0);
+CANBedDual HIGHSPEEDCAN(1);
 //CAN msgs
 unsigned char DEFAULT_MESSAGE[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 unsigned char DSC_BUTTON[8] = {0, 0, 0, 0x90, 0, 0, 0, 0};
 unsigned char LOCK_BUTTON[8] = {0, 0, 0, 0xC0, 0, 0, 0, 0};
 unsigned char UNLOCK_BUTTON[8] = {0, 0, 0, 0x84, 0, 0, 0, 0};
-/*
-FDIM_BUTTONS_ID = 0x307;
-DSC_BUTTON = 0x90;
-LOCK_BUTTON = 0xC0;
-UNLOCK_BUTTON = 0x84;
-DOMELGHT_BUTTON = 
-HAZARD_LIGHT_BUTTON = 0x01; 
-*/
+unsigned char HVAC_ON_BUTTON[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char HVAC_OFF_BUTTON[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+//RESTRAINTS CONTROL MODULE MSGS FOR CLUSTER ALARMS
+unsigned char ID341[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char ID340[8] = {0, 0x18, 0, 0, 0, 0, 0, 0};
+unsigned char ID350[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int rcm = 0;
+//////////////////////////////////////////////////////
 void setup()
 {
-  Serial.begin(115200);
-  while(!Serial);
-  while (CAN_OK != MIDSPEEDCAN.begin(CAN_125KBPS))    // init can bus : baudrate = 500k
-  {
-      Serial.println("MidSpeed CANbus Error...");
-      delay(100);
-  }
-  Serial.println("MidSpeed CANbus Connected.");
-   // Set the button pins as input with internal pull-ups
-  for (int i = 0; i < 4; i++) {
-    pinMode(buttonPins[i], INPUT_PULLUP);
-  }
+    Serial.begin(115200);
+    pinMode(18, OUTPUT);  
+    pinMode(19, INPUT_PULLUP);
+    pinMode(20, INPUT_PULLUP);
+    pinMode(21, INPUT_PULLUP);
+    pinMode(22, INPUT_PULLUP);
+    pinMode(23, INPUT_PULLUP);
+    Serial.print("pins set \r\n");
+    Wire1.setSDA(6);
+    Wire1.setSCL(7);
+    Wire1.begin();
+    Serial.print("1 wire init \r\n");
+    //SETUP CANBUS PCMCAN and ABSCAN // ABS CAN  NEEDS A TERMINATING RESISTOR 120 OHM
+    MIDSPEEDCAN.init(125000);          // CAN0 baudrate: 500kb/s
+    HIGHSPEEDCAN.init(500000);          // CAN1 baudrate: 500kb/s
+    Serial.print("can0 & can1 init \r\n");
 }
+//////////////////////////////////////////////////////
 void loop()
 {
+  unsigned long id = 0;
+  int ext = 0;
+  int rtr = 0;
+  int fd = 0;
+  int len = 0;    
+  //READ PCM CAN BUS
+  bool dscState = true; //DSC State false = off, true = on
+  bool hvacSate = false; //HVAC ON = true, off = false
   // Record the start time
   unsigned long startTime = millis();
-
-  // Check each button for a press (active LOW)
-  for (int i = 0; i < 4; i++) {
-    if (digitalRead(buttonPins[i]) == LOW) {
-      // Simple debounce delay
-      delay(50);
-      // Confirm the button is still pressed before sending the message
-      if (digitalRead(buttonPins[2]) == LOW) {
-        CAN.sendMsgBuf(0x307, 0, 8, LOCK_BUTTON);
-        // Wait until the button is released to avoid multiple sends
-        while (digitalRead(buttonPins[2]) == LOW) {
-          delay(10);
-        }
-      }
-      if (digitalRead(buttonPins[3]) == LOW) {
-        CAN.sendMsgBuf(0x307, 0, 8, UNLOCK_BUTTON);
-        // Wait until the button is released to avoid multiple sends
-        while (digitalRead(buttonPins[3]) == LOW) {
-          delay(10);
-        }
-      }
-      if (digitalRead(buttonPins[4]) == LOW) {
-      // For loop that runs for exactly 4 seconds
-        for (; millis() - startTime < 4000; ) {
-          // Code inside the loop will run for 4 seconds
-          CAN.sendMsgBuf(0x307, 0, 8, DSC_BUTTON);
-          delay(10);  // Optional delay to slow down output
-        }
-        //Light Needs to come on here
-        // Wait until the button is released to avoid multiple sends
-        while (digitalRead(buttonPins[4]) == LOW) {
-          delay(10);
-        }
-      }
+  // Check each button for a press (active LOW) 
+  if (rcm = 0) {
+   HIGHSPEEDCAN.send(0x340, 0, 0, 0, 8, ID340);
+   HIGHSPEEDCAN.send(0x340, 0, 0, 0, 8, ID350);
+   delay(50);
+   rcm = 1;
+  }
+  if (rcm = 1) {
+   HIGHSPEEDCAN.send(0x340, 0, 0, 0, 8, ID341);
+   HIGHSPEEDCAN.send(0x340, 0, 0, 0, 8, ID350);
+   delay(50);
+   rcm = 0;
+  }
+  Serial.print("Restraints Control Module Signal Sent. \r\n");
+  // Confirm the button is still pressed before sending the message
+  if (digitalRead(19) == LOW) {
+    MIDSPEEDCAN.send(0x307, 0, 0, 0, 8, LOCK_BUTTON);
+    Serial.print("LOCK_BUTTON \r\n");
+    // Wait until the button is released to avoid multiple sends
+    while (digitalRead(19) == LOW) {
+      delay(10);
+    }
+  }
+  if (digitalRead(20) == LOW) {
+    MIDSPEEDCAN.send(0x307, 0, 0, 0, 8, UNLOCK_BUTTON);
+    Serial.print("UNLOCK_BUTTON \r\n");
+    // Wait until the button is released to avoid multiple sends
+    while (digitalRead(20) == LOW) {
+      delay(10);
+    }
+  }
+  if (digitalRead(21) == LOW) {
+  // For loop that runs for exactly 4 seconds
+    if(dscState) {
+      for (; millis() - startTime < 4000; ) {
+        // Code inside the loop will run for 4 seconds
+        MIDSPEEDCAN.send(0x307, 0, 0, 0, 8, DSC_BUTTON);
+        Serial.print("DSC_BUTTON_ON\r\n");
+        delay(10);  // Optional delay to slow down output
+      }          
+      dscState = false; //DSC NOW OFF
+    }
+    else {
+      MIDSPEEDCAN.send(0x307, 0, 0, 0, 8, DSC_BUTTON);
+      Serial.print("DSC_BUTTON_OFF\r\n");
+      delay(10);  // Optional delay to slow down output  
+      dscState = true; //DSC ON 
+    }
+    //Light Needs to come on here
+    // Wait until the button is released to avoid multiple sends
+    while (digitalRead(21) == LOW) {
+      delay(10);
+    }
+  }
+  if (digitalRead(22) == LOW) {
+    MIDSPEEDCAN.send(0x307, 0, 0, 0, 8, HVAC_ON_BUTTON);
+    Serial.print("HVAC_ON_BUTTON\r\n");
+    // Wait until the button is released to avoid multiple sends
+    while (digitalRead(22) == LOW) {
+      delay(10);
+    }
+  }
+  if (digitalRead(23) == LOW) {
+    MIDSPEEDCAN.send(0x307, 0, 0, 0, 8, HVAC_OFF_BUTTON);
+    Serial.print("HVAC_OFF_BUTTON\r\n");
+    // Wait until the button is released to avoid multiple sends
+    while (digitalRead(23) == LOW) {
+      delay(10);
     }
   }
 }
-// END FILE
+////////////////////////////////////////////////////// 
+
+// ENDIF
+////////////////////////////////////////////////////////////////////////////
